908b7cad160c804041918b0d626518351e674a86
MaxiLopez <132620422+MaxilopezDAW@users.noreply.github.com>
2025-10-02 18:09:34 +0200

    BatoiBooks 1 (creación proyecto vite)

---
+++ BatoiBooks/src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <head> 
+  <a href = ..\public\logoBatoi.png >
+  <title> BatoiBooks </title>
+  </head>
+`
+
+setupCounter(document.querySelector('#counter'))
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './style.css'
+
+// Seleccionamos el contenedor principal
+const app = document.querySelector('#app')
+
+// Definimos el contenido
+app.innerHTML = `
+  <header>
+    <img src="/logoBatoi.png" alt="Logo Batoi">
+    <h1>BatoiBooks</h1>
+  </header>
+  <p>
+    Abre la consola para ver su funcionamiento
+  </p>
+`;

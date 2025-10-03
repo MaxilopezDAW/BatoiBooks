b35b6df4ad870ffdae4a2b3b189ced1b7468a295
MaxiLopez <132620422+MaxilopezDAW@users.noreply.github.com>
2025-10-03 17:37:55 +0200

    funciones

--- src/functions.js
+++ src/functions.js
@@ -1,1 +1,45 @@
-export{}
+
+getBookById => (books, bookId) => books.find(book => book.id = bookId); if(!book) throw new console.error("Book not found");
+
+getBookIndexById => (books, bookId) => books.findIndex(book => book.id = bookId); if(!book) throw new console.error("Book not found");
+
+bookExists(books, userId, moduleCode) => 
+booksFromUser(books, userId)
+booksFromModule(books, userId)
+booksCheeperThan(books, moduleCode)
+booksWithStatus(books, estado)
+averagePriceOfBooks(books)
+booksOfTypeNote(books)
+booksNotSold(books)
+incrementPriceOfbooks(books, percentage)
+getUserById(users, userId)
+getUserIndexById(users, userId)
+getUserByNickName(users, nick)
+getModuleByCode(modules, moduleCode)
+
+  
+
+
+
+
+
+
+
+export {
+    getBookById,
+    getBookIndexById,
+    bookExists,
+    booksFromUser,
+    booksFromModule,
+    booksCheeperThan,
+    booksWithStatus,
+    averagePriceOfBooks,
+    booksOfTypeNote,
+    booksNotSold,
+    incrementPriceOfbooks,
+    getUserById,
+    getUserIndexById,
+    getUserByNickName,
+    getModuleByCode 
+  }
+  
